a177abf5fc1fc8e64287239d082d5b70bc0c55a3
chaeun99 <hyc9328@naver.com>
2025-11-06 10:59:29 +0900

    test

---
+++ Template.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Template {
+    // 백준 제출 시 클래스명은 Main이어야 합니다.
+    public static void main(String[] args) throws IOException {
+        // 빠른 입출력을 위한 BufferedReader 설정
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 한 줄의 여러 데이터를 처리하기 위한 StringTokenizer 선언
+        StringTokenizer st;
+
+        // 여기에 문제 풀이 코드를 작성합니다.
+
+        br.close();
+    }
+}
